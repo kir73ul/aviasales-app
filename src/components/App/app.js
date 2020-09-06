c2054deab29a0b483ad5045c0d77d4b0e09d0fe5
Kirill <kirrfilippov@yandex.ru>
2020-09-06 14:35:21 +0400

    made adaptive layout

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'antd/dist/antd.css';
-import { Row, Col } from 'antd';
 import classes from './app.module.scss';
 import AppHeader from '../app-header';
 import LeftBar from '../left-bar';
@@ -10,14 +8,10 @@ export default function App() {
   return (
     <div className={classes.app}>
       <AppHeader />
-      <Row gutter={20} justify="center">
-        <Col span={5}>
-          <LeftBar />
-        </Col>
-        <Col span={12}>
-          <TicketsContent />
-        </Col>
-      </Row>
+      <div className={classes.flexbox} >
+        <LeftBar />
+        <TicketsContent />
+      </div>
     </div>
   );
 }
