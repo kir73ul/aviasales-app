f0c4d14d4719a0fc0af671880a5530720104ebfa
Kirill <kirrfilippov@yandex.ru>
2020-09-06 15:10:30 +0400

    fixes font, normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import 'normalize.css';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
