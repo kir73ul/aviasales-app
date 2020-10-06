a26e6bed674ed86a956205e3cfa19615100fd322
Kirill <kirrfilippov@yandex.ru>
2020-10-06 19:39:25 +0400

    separate store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducer';
+import store from './combineStore';
 import 'normalize.css';
 import App from './containers/App';
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
